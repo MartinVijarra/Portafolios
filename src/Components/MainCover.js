d3a021c2c7404aa302cab600abbc3e7d49a2e3dc
martin-vijarra <martin_vijarra@hotmail.com>
2023-02-13 03:40:01 -0300

    Nav-bar responsive sin terminar

--- src/Components/MainCover.js
+++ src/Components/MainCover.js
@@ -1,4 +1,5 @@
 import "../Style/MainCover.css"
+import Cover from "../Assets/cover.png"
 import { Link } from "react-router-dom"
 
 export function MainCover() {
@@ -8,8 +9,12 @@ export function MainCover() {
       <div className="main-container">
         <div className="main-text">
           <h1 className="main-logo">MARTIN VIJARRA</h1>
-          <p className="text-description">Desarrollador Web Front-End en HTML, CSS, Javascript y ReactJs</p>
-          <Link to="/sobremi"><button className="btn-info">VER MÁS</button></Link>
+          <p className="text-description">Desarrollador Web Front-End en HTML | CSS | Javascript | ReactJs</p>
+          <Link to="/sobremi" id="a"><button className="btn-info">Ver Más</button></Link>
+        </div>
+        
+        <div className="cover-container">
+          <img src={Cover} className="cover" alt="cover"></img>
         </div>
       </div>
     </main>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,14 +1,34 @@
 import { Link } from "react-router-dom"
+import { MenuOutlined, CloseOutlined } from '@ant-design/icons';
 import "../Style/NavBar.css"
+import { useRef } from "react";
 
 export function NavBar() {
+
+    const navRef = useRef()
+
+    const showNavBar = () =>{
+        navRef.current.classList.toggle("responsive-nav")
+    }
+
     return(
-        <div className="nav-bar">
-            <div className="menu">
-                <Link to="/" className="btn-nav">Inicio</Link>
-                <Link to="/sobremi" className="btn-nav">Sobre Mi</Link>
-                <Link to="/trabajos" className="btn-nav">Trabajos</Link>
-            </div>
-        </div>
+        <header className="nav-bar">
+            <h4 className="logo">Portfolio</h4>
+            
+            <nav className="menu-container">
+                <div className="menu">
+                    <Link to="/" className="btn-nav">Inicio</Link>
+                    <Link to="/sobremi" className="btn-nav">Sobre Mi</Link>
+                    <Link to="/trabajos" className="btn-nav">Trabajos</Link>
+                <button className="btn-bars" onClick={showNavBar}>
+                    <MenuOutlined />
+                </button>
+                </div>    
+            </nav>
+            
+                <button className="btn-bars btn-close" onClick={showNavBar}>
+                    <CloseOutlined />
+                </button>
+        </header>
     )
 }
